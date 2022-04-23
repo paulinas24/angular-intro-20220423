cd22246726232fe0b8e88114fc1ecbdaf7557d6c
paulinas24 <ps56015@gmail.com>
2022-04-23 15:14:42 +0200

    final for today

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+import { TodoComponent } from './todos/todo/todo.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path: "counter",
+component: CounterComponent,
+},
+{path: 'todos',
+component: TodosComponent,
+},
+{
+   path:'',
+  redirectTo: '',
+  pathMatch: 'full',
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
